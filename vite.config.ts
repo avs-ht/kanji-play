bbe883ef639b958bb7f339c421d604645ebaf493
avsuvorov <artyom.suvorov.shigoto@gmail.com>
2024-02-23 15:19:07 +0300

    project presetting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    TanStackRouterVite()
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, 'src'),
+      "@api": resolve(__dirname, 'src/api'),
+      "@const": resolve(__dirname, 'src/constants'),
+      "@types": resolve(__dirname, '.src/types'),
+      "@helpers": resolve(__dirname, 'src/helpers'),
+    }
+  }
 })
